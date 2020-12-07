492a8dac1db41a4da07f5e97a5cea601820ce186
xujing0312 <xujing031222@163.com>
2020-12-07 23:29:15 +0800

    first commit

---
+++ docs/.vuepress/components/my-btn.vue
@@ -0,0 +1,14 @@
+<template>
+<button @click="handleClick">可点击按钮</button> 
+</template>
+<script>
+export default{
+methods:{
+    handleClick(){
+        alert("点击成功")
+    }
+}
+}
+</script>
+<style>
+</style>
---
+++ docs/tech/README.md
@@ -0,0 +1,1 @@
+<my-btn></my-btn>
